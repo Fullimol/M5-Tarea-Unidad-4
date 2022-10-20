b3422838dbe5765c07818526151ded514fb84c21
Bruno Freijomil <bruno_freijomil@hotmail.com>
2022-10-20 18:08:56 -0300

    conexion a BD

--- app.js
+++ app.js
@@ -25,6 +25,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+//CONSULTAS ------------
+pool.query('select * from empleados').then(function (resultados) {
+  console.log(resultados)
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
